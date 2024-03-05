a90aeae0d114fc117a2040fd54629b512c88412a
shrimaliharshal <108151411+shrimaliharshal@users.noreply.github.com>
2024-03-04 16:22:56 -0800

    Update new.py

--- new.py
+++ new.py
@@ -127,3 +127,65 @@ send_data_to_kafka(suspense_df, 'redditData')
 
 kafka_df = read_data_from_kafka('redditData')
 print(kafka_df.head(5))
+
+
+def sentimentAnalysis_usingSparkNLP(df):
+    spark_df = spark.createDataFrame(posts_df)
+    
+    
+            
+    
+    prepared_df = spark_df.withColumnRenamed("selftext", "text")
+    
+    # Analyze sentiment with both pipelines
+    pipeline = PretrainedPipeline('analyze_sentimentdl_glove_imdb', 'en')
+    pipeline2 = PretrainedPipeline("analyze_sentimentdl_use_imdb", lang = "en")
+    result_df1 = pipeline.transform(prepared_df)
+    result_df2 = pipeline2.transform(prepared_df)
+    
+            # Extract sentiment and sentiment score from the first pipeline's results
+    test1 = result_df1.withColumn("sentiment1", expr("sentiment.result[0]"))
+            
+    
+            # Extract sentiment and sentiment score from the second pipeline's results
+    test2 = result_df2.withColumn("sentiment2", expr("sentiment.result[0]"))
+            
+            # Join the original DataFrame with the first pipeline's results
+    combined_df = spark_df.join(test1.select("title", "sentiment1"), "title")
+    
+            # Join the combined DataFrame with the second pipeline's results
+    final_df = combined_df.join(test2.select("title", "sentiment2"), "title")
+    return final_df
+    
+        
+
+def read_from_kafka_and_write_to_hive(posts_df,topic_name, hive_table_name, bootstrap_servers='localhost:9092', sparksession : SparkSession):
+    post_df = sparksesison \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", bootstrap_servers) \
+        .option("subscribe", topic_name) \
+        .load()
+    
+    
+    schema = StructType([
+        StructField("title", StringType(), True),
+        StructField("sentiment1", StringType(), True),
+        StructField("sentiment2", StringType(), True),
+        
+    ])
+    
+    df = df.selectExpr("CAST(value AS STRING)") \
+           .select(from_json(col("value"), schema).alias("data")) \
+           .select("data.*")
+    
+    # Writing to Hive in Parquet format
+    df.writeStream \
+      .format("parquet") \
+      .option("path", "path/to/hive/table/location") \
+      .option("checkpointLocation", "path/to/checkpoint/dir") \
+      .start(hive_table_name) \
+      .awaitTermination()
+
+
+read_from_kafka_and_write_to_hive(kafka_df,reddit_topic,reddit_table)
